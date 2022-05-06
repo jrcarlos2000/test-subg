e6783d951c82df323fc3a7762a0f74af99298333
Lay90 <liqw19@mails.tsinghua.edu.cn>
2022-05-06 12:24:12 +0800

    fix(main): change tokenAddr2Symbol to map instead of dictionary

--- src/tokenList.ts
+++ src/tokenList.ts
@@ -1,14 +1,25 @@
-const tokenAddr2Symbol: {[key: string]: string} = {
-    "0x7a96316B13bD7d0529e701d2ED8b9fC4E4fd8696" : "USDC",
-    "0x158653b66fd72555F68eDf983736781E471639Cc" : "USDT",
-    "0xeBC8428DC717D440d5deCE1547456B115b868F0e" : "WETH",
-    "0x05d6f705C80d9F812d9bc1A142A655CDb25e2571" : "WBTC",
-    "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889" : "WMATIC"
-}
+// interface dict {
+//     [key: string]: string;
+// }
 
-export default function getTokenSymbol(tokenAddress: string): string {
-    if (tokenAddress in tokenAddr2Symbol) {
-        return tokenAddr2Symbol[tokenAddress];
+// const tokenAddr2Symbol: dict = {
+//     "0x7a96316B13bD7d0529e701d2ED8b9fC4E4fd8696" : "USDC",
+//     "0x158653b66fd72555F68eDf983736781E471639Cc" : "USDT",
+//     "0xeBC8428DC717D440d5deCE1547456B115b868F0e" : "WETH",
+//     "0x05d6f705C80d9F812d9bc1A142A655CDb25e2571" : "WBTC",
+//     "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889" : "WMATIC"
+// }
+
+let tokenAddr2Symbol = new Map<string, string>();
+tokenAddr2Symbol.set("0x7a96316B13bD7d0529e701d2ED8b9fC4E4fd8696", "USDC");
+tokenAddr2Symbol.set("0x158653b66fd72555F68eDf983736781E471639Cc", "USDT");
+tokenAddr2Symbol.set("0xeBC8428DC717D440d5deCE1547456B115b868F0e", "WETH");
+tokenAddr2Symbol.set("0x05d6f705C80d9F812d9bc1A142A655CDb25e2571", "WBTC");
+tokenAddr2Symbol.set("0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", "WMATIC");
+
+export function getTokenSymbol(tokenAddress: string): string {
+    if (tokenAddr2Symbol.has(tokenAddress)) {
+        return tokenAddr2Symbol.get(tokenAddress);
     } else {
         return "UNSUPPORTED_TOKEN";
     }
--- src/vaultMapping.ts
+++ src/vaultMapping.ts
@@ -24,39 +24,10 @@ import {
   UpdatePosition
 } from "../generated/Vault/Vault"
 import { ActivePosition, Transaction } from "../generated/schema"
-import getTokenSymbol from "./tokenList"
+import { getTokenSymbol } from "./tokenList"
 
 
-export function handleBuyUSDG(event: BuyUSDG): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  // let entity = ActivePosition.load(event.transaction.from.toHex())
-
-  // // Entities only exist after they have been saved to the store;
-  // // `null` checks allow to create entities on demand
-  // if (!entity) {
-  //   entity = new ActivePosition(event.transaction.from.toHex())
-
-  //   // Entity fields can be set using simple assignments
-  //   entity.count = BigInt.fromI32(0)
-  // }
-
-  // // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1)
-
-  // // Entity fields can be set based on event parameters
-  // entity.account = event.params.account
-  // entity.token = event.params.token
-
-  // // Entities can be written to the store with `.save()`
-  // entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-}
+export function handleBuyUSDG(event: BuyUSDG): void {}
 
 export function handleClosePosition(event: ClosePosition): void {
   // remove ActivePosition
